5f61e9754174b11af05812307c0ec5f3c7870ec5
Birdtr222 <jungin996@naver.com>
2023-05-11 10:31:07 +0900

    05.11 수정 업데이트

--- pages/Searchpage.js
+++ pages/Searchpage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Component } from 'react';
-import { Alert, Dimensions, TouchableOpacity, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { TextInput, Alert, Dimensions, TouchableOpacity, StyleSheet, Text, View, ScrollView } from 'react-native';
 import * as Location from "expo-location"
 
 
@@ -14,25 +14,70 @@ import { Ionicons } from '@expo/vector-icons';
 const screenWidth = Dimensions.get('window').width
 
 
-export default function Searchpage () {
-    //스크린 사이즈에 맞게 설정
+export default function Searchpage ({navigation,route}) {
+
+    //useState 사용법
+	//[state,setState] 에서 state는 이 컴포넌트에서 관리될 상태 데이터를 담고 있는 변수
+  //setState는 state를 변경시킬때 사용해야하는 함수
+  //날씨 데이터 상태관리 상태 생성!
+  const [weather, setWeather] = useState({
+    temp : 0,
+    condition : ''
+  })
+
+
+
+  const getLocation = async () => {
+    //수많은 로직중에 에러가 발생하면
+    //해당 에러를 포착하여 로직을 멈추고,에러를 해결하기 위한 catch 영역 로직이 실행
+    try {
+      //자바스크립트 함수의 실행순서를 고정하기 위해 쓰는 async,await
+      await Location.requestForegroundPermissionsAsync();
+      const locationData= await Location.getCurrentPositionAsync();
+      const latitude = locationData['coords']['latitude']
+      const longitude = locationData['coords']['longitude']
+      const API_KEY = "cfc258c75e1da2149c33daffd07a911d";
+      const result = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+      );
+
+      const temp = result.data.main.temp; 
+      const condition = result.data.weather[0].main
+
+      //오랜만에 복습해보는 객체 리터럴 방식으로 딕셔너리 구성하기!!
+      //잘 기억이 안난다면 1주차 강의 6-5를 다시 복습해보세요!
+      setWeather({
+        temp, condition
+      })
+
+    } catch (error) {
+      //혹시나 위치를 못가져올 경우를 대비해서, 안내를 준비합니다
+      Alert.alert("위치를 찾을 수가 없습니다.", "앱을 껏다 켜볼까요?");
+    }
+  }
+  // 검색창 공란으로 변하기
+  const [number, onChangeNumber] = React.useState('');
+
 
     return (
     <View style ={styles.container}>    
         <View style={styles.header}><MaterialCommunityIcons name="weather-pouring" size={120} color="black" />
-        <Text style={styles.text}> 현재온도: 16 °C</Text>
-        <Text>체감온도: 16 °C 습도 73% 동풍 1m/s</Text>
-        <Text>미세먼지: 좋음 초미세먼지: 좋음 자외선: 좋음</Text>
+        <Text style={styles.text}> 현재온도: {weather.temp + '°C' + weather.condition}</Text>
+        <Text>체감온도: ?? °C 습도 ??% 동풍 ?? m/s</Text>
+        <Text>미세먼지: ?? 초미세먼지: ?? 자외선: ??</Text>
         </View>
         <Text style={styles.textTag}>태그 검색</Text>
         <View style={styles.Content01}>
+             <TouchableOpacity>
+                <TextInput style ={{height:40, marginLeft:20, fontSize:25}} onChangeText={onChangeNumber} value={number} placeholder="useless placeholder"/>
+             </TouchableOpacity>
             <TouchableOpacity>
             <FontAwesome5 name="search" size={30} color="black" margin={10}/>
             </TouchableOpacity>
             </View>
         <View style={styles.Content02}>
         <ScrollView>
-            <Text>이곳은 위아래 스크롤 뷰 입니다. 검색 전 초기 데이터들 그냥 일부분 보여줄 수 있는 곳이에요. 가이드라인만 잡고 있는 중입니다.. 아직 세팅은 하지 않았어요...가이드라인 사진은 이미지로 보여드릴게요 유후링..</Text>
+            <Text>데이터 흩뿌리는 곳</Text>
         </ScrollView>
         </View>
         {/** 아이콘 들 */}
@@ -84,17 +129,17 @@ const styles = StyleSheet.create({
     Content01:{
         flex:1,
         margin:10,
-        alignContent:'flex-end',
-        alignItems:'flex-end',
-        justifyContent:'center',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:"space-between",
         borderRadius:40,
         borderWidth:3,
-        borderColor:'black'
+        borderColor:'black',
     },
     Content02:{
         flex:4,
         margin:10,
-        backgroundColor:'purple'
+        backgroundColor:'yellow'
     },
     footer:{
         flex:1,
