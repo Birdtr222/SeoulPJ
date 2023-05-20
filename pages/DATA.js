a0fd717a45f9d1c81b578572c14c088fa816149a
Birdtr222 <jungin996@naver.com>
2023-05-20 13:14:54 +0900

    데이터 시도 파일 업로드

---
+++ pages/DATA.js
@@ -0,0 +1,35 @@
+// 작성자 : 정혜윤
+// 날짜 : 05.15
+// 기능 : 데이터 불러오기
+
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
+// import requests
+// import pandas as pd 
+
+// export default function Loading() {
+//     return <View style ={styles.container}><Text style={styles.text}> 잠시만 기다려주세요... </Text></View>
+// }
+
+// const styles = StyleSheet.create({
+//     container:{
+//         flex:1,
+//         justifyContent : "center",
+//         alignItems :"center",
+//         backgroundColor: '#B0DAFF'
+//     },
+//     text:{
+//         color: '#146C94',
+//         fontSize: 30
+//     }
+
+//     const URL = "http://openapi.seoul.go.kr:8088/6a6675764a6a6a733635674d755652/xml/walkDoseongInfo/1/5/";
+
+// const response = axios.get(URL, {
+//     params: {
+//       serviceKey: process.env.REACT_APP_API_KEY,
+//       numOfRows: 1,
+//       pageNo: 10
+//     }
+// });
+// })
